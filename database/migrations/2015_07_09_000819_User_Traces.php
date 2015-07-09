8dde731e6e87ad134d436700b08011f6960f633f
Dustyn <djtubbs1@svsu.edu>
2015-07-08 19:11:54 -0500

    Created the migration to describe user_traces
    User traces can be thought of as the traces that belong to a particular user.
    
    This schema supports the notion that a user can have multiple trace files and that file is bound to that user.

---
+++ database/migrations/2015_07_09_000819_User_Traces.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTraces extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_traces', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('uuid')->unique();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_traces');
+    }
+}
