bc3800b30696df19c01de37551fbd9f3f9d9ece1
Dustyn <djtubbs1@svsu.edu>
2015-07-15 21:51:52 -0500

    Changed styling for user menu to have a chevron hide and show the menu respectively

--- public/js/menus.js
+++ public/js/menus.js
@@ -2,9 +2,10 @@
 
 var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
     showLeft = document.getElementById( 'showLeft' ),
+    hideLeft = document.getElementById('hideLeft')
     body = document.body;
 
-showLeft.onclick = function() {
+showLeft.onclick = hideLeft.onclick = function() {
   classie.toggle( this, 'active' );
   classie.toggle( menuLeft, 'cbp-spmenu-open' );
 };
