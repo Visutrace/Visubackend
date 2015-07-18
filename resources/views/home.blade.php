0102e605735107c4b1304ad5fe457bc3e38ad89b
Dustyn <djtubbs1@svsu.edu>
2015-07-18 11:47:25 -0500

    Home page mockup for user traces

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,32 @@
 @extends('templates.base')
 @section('content')
 @include('snippets.usermenu')
+
+
+<div class="col-xs-12">
+  @foreach($user->traces as $iterator=>$trace)
+
+    @if($iterator % 3 === 0)
+      <div class="col-xs-12"><br/></div>
+
+
+        <div class="col-xs-offset-3 col-xs-3">
+
+        <a href="{!! route('traces.show',$trace->uuid) !!}">
+            <img src="http://dummyimage.com/200x200/000/fff.png&txt={!! $trace->uuid !!}">
+        </a>
+
+        </div>
+
+    @else        
+        <div class="col-xs-3">
+
+        <a href="{!! route('traces.show',$trace->uuid) !!}">
+            <img src="http://dummyimage.com/200x200/000/fff.png&txt={!! $trace->uuid !!}">
+        </a>
+
+        </div>
+    @endif
+  @endforeach
+</div>
 @stop
