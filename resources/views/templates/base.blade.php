56c7aa89a06d23779ce50029a03afd4b6d9563b2
Dustyn <djtubbs1@svsu.edu>
2015-08-04 17:28:56 -0400

    Removed un used footers

--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -6,16 +6,8 @@
 {!! HTML::style('css/template_style.css') !!}
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css">
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-<!--
-corps, a free CSS web template by ZyPOP (zypopwebtemplates.com/)
 
-Download: http://zypopwebtemplates.com/
 
-License: Creative Commons Attribution
-//-->
 </head>
 <body>
 <div id="container">
@@ -73,52 +65,12 @@ License: Creative Commons Attribution
 </div>
 
 @endif
-<div class="col-xs-12">
+<div class="container">
     
     @yield('content')
 
 </div>
 
-
-
-    <footer class="col-xs-12">
-    <!--
-        <div class="footer-content width">
-            <ul>
-                <li><h4>Proin accumsan</h4></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Blandit elementum</a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-            </ul>
-            
-            <ul>
-                <li><h4>Condimentum</h4></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Cras dictum</a></li>
-            </ul>
-            
-            <ul class="endfooter">
-                <li><h4>Suspendisse</h4></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Donec in ligula nisl.</a></li>
-            </ul>
-            
-            <div class="clear"></div>
-        </div>
-        -->
-        <div class="footer-bottom">
-            <p>&copy; Visutrace 2015.</p>
-            <p>Like the style of this site? We did too. Check out ZyPOP for other <a href="http://zypopwebtemplates.com/">free CSS Website Templates</a> by ZyPOP</p>
-         </div>
-    </footer>
 </div>
 </body>
 </html>
