c12d605b94785f03528062685a4f249784c4459a
Dustyn <djtubbs1@svsu.edu>
2015-08-04 17:28:29 -0400

    added styling to make the menu chevron not look dumb

--- resources/views/snippets/usermenu.blade.php
+++ resources/views/snippets/usermenu.blade.php
@@ -6,17 +6,11 @@
   </h3>
   <a href="{!! route('home') !!}">Home</a>
   <a href="{!! route('traces.create') !!}">Upload a data set</a>
-  <a href="#">Zucchini garlic</a>
-  <a href="#">Catsear azuki bean</a>
-  <a href="#">Dandesdsdsdsdlion bunya</a>
-  <a href="#">Rutabaga</a>
 </nav>
 
-  <div>
+  <div style="position:fixed; left:10px">
      <h3 class="white-chevron"><i class="fa fa-chevron-right" id="showLeft"></i></h3>
   </div>
-
-
 
 
 {!! HTML::style('css/push_left_menu.css') !!}
