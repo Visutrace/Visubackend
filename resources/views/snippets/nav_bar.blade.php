458b8a7b9eced9dc9db7e63b238fe0fbdcc56dee
Dustyn <djtubbs1@svsu.edu>
2015-08-04 17:28:02 -0400

    removed old template links

--- resources/views/snippets/nav_bar.blade.php
+++ resources/views/snippets/nav_bar.blade.php
@@ -1,11 +1,7 @@
-    <nav>
-    <div class="width">
-            <ul>
-                <li class="start selected"><a href="index.html">Home</a></li>
-                    <li class=""><a href="examples.html">Examples</a></li>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Solutions</a></li>
-                <li class="end"><a href="#">Contact</a></li>
-            </ul>
+    <div class="col-xs-12">
+        <nav>
+        <div class="width">
+        </div>
+        </nav>
     </div>
-    </nav>
+
