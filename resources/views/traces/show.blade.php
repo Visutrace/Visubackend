7cd7edb3b7f2846b3b304b453e75393c2a0598e5
Dustyn <djtubbs1@svsu.edu>
2015-08-04 17:29:16 -0400

    Displaying data correctly, finally

--- resources/views/traces/show.blade.php
+++ resources/views/traces/show.blade.php
@@ -2,10 +2,38 @@
 @section('page_content')
 
 
-<div class="col-xs-offset-6 col-xs-6">
+{!! HTML::style('css/theatre.css') !!}
+
+
+<div class="visualization-contiainer">
     
-    <h1> Display the traces here!</h1>
+        <div id="dat-gui-container" class="col-xs-offset-8 col-xs-4">
+        </div>
+
+        <div class = "col-xs-12">
+              <canvas id="display-canvas" class="canvas"></canvas>
+        </div>  
 
 </div>
+
+<script type="text/javascript">
+
+    <?php 
+
+        $trace_data = json_encode($trace_lines);
+        echo 'var tracedata = '.$trace_data.";\n";
+     ?>
+
+</script>
+
+{!! HTML::script('js/jquery-1.11.0.js') !!}
+{!! HTML::script('js/jquery.scrollTo.min.js') !!}
+{!! HTML::script('js/bootstrap.min.js') !!}
+{!! HTML::script('js/dat.gui.min.js') !!}
+{!! HTML::script('js/BuildDatGui.js') !!}
+{!! HTML::script('js/pixi.js') !!}
+{!! HTML::script('js/VisuTrace.js') !!}
+{!! HTML::script('js/DisplayEngine.js') !!}
+
 
 @stop
