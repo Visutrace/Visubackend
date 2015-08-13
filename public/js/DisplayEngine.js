8aecf8f0412218d01c033c6f1fc8a474a1bb0b58
Dustyn <djtubbs1@svsu.edu>
2015-08-13 13:59:33 -0400

    Changed origin to be bottom left and removed unecessary menu functionality

--- public/js/DisplayEngine.js
+++ public/js/DisplayEngine.js
@@ -19,6 +19,11 @@
 	//First thing's first, we need to parse through all the trace data
 	//	and make unique paths
 
+// var tracedata = [
+// "00000.00,547,0,0,100,100,2.00",
+// "00010.00,547,100,100,100,200,3.78",
+// "00020.00,547,100,200,200,200,4.22",
+// ]
 
 	var allPaths =   []; //Where the final paths will be sent
 
@@ -27,7 +32,11 @@
 	for(var i = 0; i < numOfLengths; i++){ //for each line of trace data
 		pD = tracedata[i].split(","); //split the values
 
-		temp = new Length(pD[0], pD[1],pD[2],pD[3],pD[4],pD[5],pD[6]); //make the values into a length called temp
+
+
+		temp = new Length(pD[0], pD[1], pD[2], 3000-pD[3],pD[4], 3000-pD[5],pD[6]); //make the values into a length called temp
+
+		console.log(temp);
 
 		if(allPaths[temp.id]){ //If there is a path by the id of this length
 			allPaths[temp.id].addLength(temp); //add the length to that path
@@ -130,25 +139,25 @@
 	 	now = Date.now();
 		time = now-start;	
 
-	//for each car, drive!
-	cars.forEach(function(car){
-		car.setScale(car.defaultScale);
-		car.drive(time);
-		if(car.finishedMoving){
-			car.setColor(0x808080);
-			if(car.kset.length > 0){
-				car.kr2 = time;
+		//for each car, drive!
+		cars.forEach(function(car){
+			car.setScale(car.defaultScale);
+			car.drive(time);
+
+			if(car.finishedMoving){
+				car.setColor(0x808080);
+			} else {
+				car.setColor(car.internalColor) ;
 			}
-		} else {
-			car.setColor(car.internalColor) ;
-		}
+			
+
 		if(car.isActive){
 			activeVehicle = car;
 			
-			externalIdController.onFinishChange(function(value){
-				activeVehicle.setExternalID(value);
-			});
-			//resets activity graphics
+			// externalIdController.onFinishChange(function(value){
+			// 	activeVehicle.setExternalID(value);
+			// });
+
 			for (i = activeVehicle.activityLayer.children.length - 1; i >= 0; i--) {
 				activeVehicle.activityLayer.removeChild(activeVehicle.activityLayer.children[i]);
 			}
@@ -167,184 +176,19 @@
 			graphics.beginFill(activeVehicle.internalColor);
 			graphics.drawCircle(activeVehicle.getPosition()[0],activeVehicle.getPosition()[1],20);
 			
-			menu.internalID = activeVehicle.internalID;
-			menu.externalID = activeVehicle.externalID;
+			selectedVehicle.addChild(graphics);
+
+			menu.CarID = activeVehicle.internalID;
+			// menu.externalID = activeVehicle.externalID;
 			menu.x = parseInt(activeVehicle.getPosition()[0]);
 			menu.y = parseInt(activeVehicle.getPosition()[1]);
-			var k = parseInt( activeVehicle.get_k(activeVehicle) );
-			menu.k = k
-			menu.d = parseInt( activeVehicle.get_d(activeVehicle) );
-			if(activeVehicle.kr != null){
-				menu.t = parseInt( activeVehicle.get_t(activeVehicle,time) );
-			}else{
-				menu.t = parseInt(0);
-			}
-			menu.e = Math.log2(k)
-			menu.tp = (1/k)*100
-			if(activeVehicle.finishedMoving){
-				activeVehicle.removeActivity(activeVehicle);
-			}
-			
-			//////////////////////////////////////////////////////////
-			///////////Layers for mixed cars./////////////
-			////////////////////////////////////////////////////////
-			if(activeVehicle.kset.length > 0){
-				for (var i = carLayerStage.children.length - 1; i >= 0; i--) {
-					carLayerStage.removeChild(carLayerStage.children[i]);
-				};
-				var color = activeVehicle.kcolor;
-				graphics.lineStyle(5,color,1);
-				graphics.beginFill(color,0.3);
-				var layerlog = [];
-				activeVehicle.kset.forEach(function(c){
-					if(activeVehicle != c){
-						if(layerlog.indexOf(c) == -1){
-							graphics.drawCircle(c.getPosition()[0],c.getPosition()[1],30);
-							layerlog.push(c);
-						}
-					}
-				});
-			}
-			selectedVehicle.addChild(graphics);
-			selectedVehicle.addChild(activeVehicle.sprite);
-		}else{
-		}
-	
-		//Zone + Car interaction
-		if(allZones.length != 0){
-			allZones.forEach(function(zone){
-				//zone.updateTime(time);
-				if(zone.isOn){
-				
-					//car and zone grouping functions
-					if(zone.getDistanceFromCar(car) < zone.r){
-						if(car.currentZone == null){
-							if(car.pastZones.indexOf(zone) == -1){
-								zone.carEnter(car,time);
-							}
-						}
-					}else{
-						if(car.pastZones.indexOf(zone) != -1){
-							car.pastZones.splice(car.pastZones.indexOf(zone),1);
-						}
-					}
-					
-					//Car and zone mixing functions
-					if((time - zone.lastMix) >= zone.tping){
-						if(zone.kset.length > 0){
-							zone.mixCars(zone,time);
-						}else{
-							zone.lastMix = time;
-						}
-					}
-					//zone Display
-					var newtext = parseFloat((time - zone.lastMix)/1000).toFixed(1);
-					zone.redrawText(newtext);
-				}
-			});
+
 		}
 	});
 		
-		
-		
-		//Active Connection Functions
-		/*
-		//connect / reconnect ranked cars to active
-		var rankAverage = null;
-		var rankCount = 0;
-		cars.forEach(function(car){
-			if(car.isActive){
-				if(distRankings.length != 0){
-					rank1 = distRankings[0];
-					rank1.setScale(3);
-					rank1.setColor(0xFF0000);
-					rank1.drawConnection(rank1,car,4,0xFF0000);
-					rankAverage += rank1.getDistanceFrom(car);
-					rankCount ++;
-					if(distRankings.length > 1){
-						rank2 = distRankings[1];
-						rank2.setScale(2.5);
-						rank2.setColor(0x0000FF);
-						rank2.drawConnection(rank2,car,4,0x0000FF);
-						rankAverage += rank2.getDistanceFrom(car);
-						rankCount ++;
-						if(distRankings.length > 2){
-							rank3 = distRankings[2];
-							rank3.setScale(2);
-							rank3.setColor(0x00FF00);
-							rank3.drawConnection(rank3,car,4,0x00FF00);
-							rankAverage += rank3.getDistanceFrom(car);
-							rankCount ++;
-						}
-					}
-				}
-			}
-		});
-		*/
-		//Zone Functions
-		allZones.forEach(function(zone){
-			//Zone Interaction Function
-			if(zone.isActive){
-				var activeZone = zone;
-				menu.activeZoneId = activeZone.id;
-				menu.activeZonePingTime = activeZone.tping;
-				menu.activeZoneDeltaTime = activeZone.tdelta;
-				menu.activeZoneRadius = activeZone.r;
-				menu.activeZoneX = activeZone.x;
-				menu.activeZoneY = activeZone.y;
-				menu.activeZoneY = activeZone.y;
-					//////////////////
-				activeZonePingTimeController.onFinishChange(function(value){
-					var newvalue= parseInt(value);
-					activeZone.setPingTime(newvalue);
-				});
-					////////////////////////////////
-				activeZoneRadiusController.onFinishChange(function(value){
-					var newvalue= parseInt(value);
-					activeZone.setRadius(newvalue);
-					activeZone.redrawGraphics();
-				});
-					////////////////////////
-				activeZoneXController.onFinishChange(function(value){
-					var newvalue= parseInt(value);
-					activeZone.setPosition(newvalue,activeZone.y);
-					activeZone.redrawGraphics();
-				});
-				activeZoneYController.onFinishChange(function(value){
-					var newvalue = parseInt(value);
-					activeZone.setPosition(activeZone.x,newvalue);
-					activeZone.redrawGraphics();
-				});
-			}
-			if(zone.lift){
-				var liftZone = zone;
-				var mousePos = stage.getMousePosition();
-				liftZone.setPosition(mousePos.x ,mousePos.y);
-				liftZone.setLineColor(0xFF0000);
-				liftZone.redrawGraphics();
-				zone.reset = true;
-			}
-			else if(zone.resize){
-				var resizeZone = zone;
-				var mousePos = stage.getMousePosition();
-				var xdist = mousePos.x - resizeZone.x;
-				var ydist = mousePos.y - resizeZone.y;
-				var radius = (xdist + ydist) / 2;
-				resizeZone.setRadius(Math.ceil(radius));
-				resizeZone.setResizePosition(mousePos.x,mousePos.y);
-				resizeZone.setLineColor(0x0000FF);
-				resizeZone.redrawGraphics();
-				zone.reset = true;
-			}
-			else if(zone.reset){
-				zone.setLineColor(0x00FF00);
-				zone.setResizePosition((zone.x+zone.r)-(zone.r/3),(zone.y+zone.r)-(zone.r/3));
-				zone.redrawGraphics();
-				zone.reset = false;
-			}
+
 	
-	});
-		
+	
 		
 		stage.removeChild(movingCarsStage);
 		stage.addChild(movingCarsStage);
@@ -352,6 +196,7 @@
 		stage.swapChildren(movingCarsStage,zoneStage);
 		stage.addChild(carLayerStage);
 		stage.swapChildren(carLayerStage,zoneStage);
+
 
 	    renderer.render(stage);
 	    requestAnimFrame( update );
